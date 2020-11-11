ec6f34538bb548e0df7a66405cdce40329f69e34
weslynSouza <wesley.i476@gmail.com>
2020-11-11 10:50:33 -0300

    first commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>(
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum accusantium voluptatibus iste ullam sunt officia dolorum quo repellendus culpa possimus perspiciatis, deserunt facere eum inventore libero praesentium laboriosam ipsa sed!</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae iure suscipit mollitia nesciunt blanditiis ipsum, non, alias nostrum recusandae quidem error odio tenetur beatae maiores laudantium. Accusamus facilis qui tempora.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, incidunt harum in odit delectus sint modi fuga dolorem facere temporibus molestiae ab commodi voluptate voluptates voluptas alias ad consequatur ipsa!</p>
+    </div>
+)
